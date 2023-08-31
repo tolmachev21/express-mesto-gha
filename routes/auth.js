cc20ca57d09ac218708914939bfbd6c0d119e3d1
Viktor Tolmachev <Tolmach0221@yandex.ru>
2023-08-31 16:03:29 +0500

    Первая интерация 2

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().url(),
+    avatar: Joi.string(),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,7 @@ router.patch('/me', celebrate({
 
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required().uri(),
+    avatar: Joi.string().required(),
   }),
 }), updateAvatar);
 
