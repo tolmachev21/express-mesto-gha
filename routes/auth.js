34d1e1757560e65fdffd8adad14ab9ead3a7e182
Viktor Tolmachev <Tolmach0221@yandex.ru>
2023-09-01 09:44:29 +0500

    первая интерация 5

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,9 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
+    avatar: Joi.string().uri({
+      allowRelative: true,
+    }),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
